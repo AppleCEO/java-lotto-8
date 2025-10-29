2c7bad12254f4d9a92fa24c46609b8d113a327d0
도미닉 <kiljh@me.com>
2025-10-29 23:29:57 +0900

    feat: 숫자로 나눠서 몇 개를 구매했는지 출력하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,7 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        ConsoleReader.readMoney();
+        int money = ConsoleReader.readMoney();
+        System.out.println(money/1000 + "개를 구매했습니다.");
     }
 }
