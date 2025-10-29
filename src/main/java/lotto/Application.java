3c458e846f92f65098f8b95fa0d2c3d8f0aedf23
도미닉 <kiljh@me.com>
2025-10-29 21:39:07 +0900

    refactor: 별도 객체를 새성해서 안내 문자열 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,6 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("구입금액을 입력해 주세요.");
+        ConsoleReader.readMoney();
     }
 }
---
+++ src/main/java/lotto/ConsoleReader.java
@@ -0,0 +1,9 @@
+package lotto;
+
+import java.util.Scanner;
+
+public class ConsoleReader {
+    public static void readMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+}
