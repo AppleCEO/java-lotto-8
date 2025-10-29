d8e97247fc4d1f12cad62d3b9c426f5d1f52ff93
도미닉 <kiljh@me.com>
2025-10-29 22:23:12 +0900

    feat: 구입 금액을 문자열로 입력 받아서 숫자로 변환하여 반환하는 기능 구현

--- src/main/java/lotto/ConsoleReader.java
+++ src/main/java/lotto/ConsoleReader.java
@@ -1,9 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.Scanner;
 
 public class ConsoleReader {
-    public static void readMoney() {
+    public static int readMoney() {
         System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 }
