b46ee42aff5e144c5fd19a556e39b8a46798eb71
도미닉 <kiljh@me.com>
2025-10-29 23:32:47 +0900

    refactor: 출력하는 코드를 ConsoleOutput 객체로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,6 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         int money = ConsoleReader.readMoney();
-        System.out.println(money/1000 + "개를 구매했습니다.");
+        ConsoleOutput.printLottoAmount(money);
     }
 }
--- src/main/java/lotto/ConsoleOutput.java
+++ src/main/java/lotto/ConsoleOutput.java
@@ -4,4 +4,8 @@ public class ConsoleOutput {
     public static void printMoneyInputGuide() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public static void printLottoAmount(int money) {
+        System.out.println(money/1000 + "개를 구매했습니다.");
+    }
 }
