3f2d435b7f9d8ed2d1d366b6e1e5a67ebe99486f
도미닉 <kiljh@me.com>
2025-10-29 21:55:25 +0900

    test: 구입 금액 입력 테스트

---
+++ src/test/java/lotto/ConsoleReaderTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ConsoleReaderTest extends NsTest {
+    @Test
+    void 구입_금액_입력_테스트() {
+        assertSimpleTest(() -> {
+            run("8000", "1,2,3,4,5,6", "7");
+            int money = ConsoleReader.readMoney();
+            assertThat(money).isEqualTo(8000);
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
