cb28e6de21445034e64748d17ebbc563cd4a8994
도미닉 <kiljh@me.com>
2025-10-30 18:10:44 +0900

    feat: 입력한 로또 당첨 번호 문자열을 List<Integer>로 변환하는 기능

--- src/main/java/lotto/ConsoleReader.java
+++ src/main/java/lotto/ConsoleReader.java
@@ -2,12 +2,23 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class ConsoleReader {
     public static int readMoney() {
         ConsoleOutput.printMoneyInputGuide();
         String input = Console.readLine();
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> readWinningNumber() {
+        String input = Console.readLine();
+        List<Integer> numbers = Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return numbers;
     }
 }
