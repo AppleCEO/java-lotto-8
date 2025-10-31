44f11fefe91d5467c551d77fae8ea92452afab81
도미닉 <kiljh@me.com>
2025-10-31 23:49:57 +0900

    feat: 예시와 똑같이 출력되도록 문자열에 줄 넘김 추가

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -2,9 +2,9 @@ package lotto;
 
 public enum Message {
     PROMPT_BUY_MONEY("구입금액을 입력해 주세요."),
-    PROMPT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    PROMPT_BONUS_NUMBERS("보너스 번호를 입력해 주세요."),
-    RESULT_BUY_LOTTO_AMOUNT("\n%d개를 구매했습니다.");
+    PROMPT_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    PROMPT_BONUS_NUMBERS("\n보너스 번호를 입력해 주세요."),
+    RESULT_BUY_LOTTO_AMOUNT("\n%d개를 구매했습니다.\n");
 
     private final String message;
 
