827780217397c600dd6558854ed46f28041e46da
도미닉 <kiljh@me.com>
2025-11-01 22:24:04 +0900

    refactor: 읽는 기능들을 별도 메서드로 추출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,14 +4,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Application {
+    private static int money;
+    private static List<Lotto> lottos;
+    private static List<Integer> winningNumbers;
+    private static int bonusNumber;
+
     public static void main(String[] args) {
-        int money = ConsoleReader.readMoney();
-        List<Lotto> lottos = LottoFactory.createLottos(money);
-        List<Integer> winningNumbers = ConsoleReader.readWinningNumbers();
-        int bonusNumber = ConsoleReader.readBonusNumber();
+        readFromConsole();
+        lottos = LottoFactory.createLottos(money);
         List<Ranking> rankings = lottos.stream()
                 .map(lotto -> lotto.checkRanking(winningNumbers, bonusNumber))
                 .toList();
         ConsoleOutput.printResult();
+    }
+
+    private static void readFromConsole() {
+        money = ConsoleReader.readMoney();
+        winningNumbers = ConsoleReader.readWinningNumbers();
+        bonusNumber = ConsoleReader.readBonusNumber();
     }
 }
