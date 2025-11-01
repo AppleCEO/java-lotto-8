18ab80137be1cedb34ad5b1334664837d14b894a
도미닉 <kiljh@me.com>
2025-11-01 21:13:32 +0900

    feat: 전달받은 값을 바로 할당하면 변경이 불가하여 새로운 리스트를 만들어 할당하기

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,9 +9,9 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        Collections.sort(numbers);
-        this.numbers = numbers;
-        ConsoleOutput.printNumbers(numbers);
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
+        ConsoleOutput.printNumbers(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
