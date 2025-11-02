a1bd631f4bbf6755c38b3f7a0e866e57ec1022af
도미닉 <kiljh@me.com>
2025-11-02 20:26:02 +0900

    feat: 요구사항에 맞춰서 줄바꿈 추가

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -5,7 +5,7 @@ public enum Message {
     PROMPT_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
     PROMPT_BONUS_NUMBERS("\n보너스 번호를 입력해 주세요."),
     RESULT_BUY_LOTTO_AMOUNT("\n%d개를 구매했습니다.\n"),
-    RESULT_TITLE("당첨 통계\n---"),
+    RESULT_TITLE("\n당첨 통계\n---"),
     RESULT_RANKING_FIFTH_AMOUNT("3개 일치 (5,000원) - %d개\n"),
     RESULT_RANKING_FOURTH_AMOUNT("4개 일치 (50,000원) - %d개\n"),
     RESULT_RANKING_THIRD_AMOUNT("5개 일치 (1,500,000원) - %d개\n"),
