e4a04ca0dc9d58e8bbd0afd18e6632f248d2f93b
도미닉 <kiljh@me.com>
2025-11-03 14:10:20 +0900

    test: 입력한 로또 당첨번호에 중복된 숫자가 있는 경우 에러

--- src/test/java/lotto/ConsoleReaderTest.java
+++ src/test/java/lotto/ConsoleReaderTest.java
@@ -31,6 +31,16 @@ class ConsoleReaderTest extends NsTest {
     }
 
     @Test
+    void 로또_당첨번호_중복된_숫자_예외_테스트() {
+        assertSimpleTest(() -> {
+            run("8000", "1,2,3,4,5,5", "7");
+            int money = ConsoleReader.readMoney();
+            List<Integer> lottoWinningNumbers = ConsoleReader.readWinningNumbers();
+            assertThat(output()).contains(ErrorMessage.DUPLICATE_WINNING_NUMBERS.getMessage());
+        });
+    }
+
+    @Test
     void 보너스_번호_입력_테스트() {
         assertSimpleTest(() -> {
             run("8000", "1,2,3,4,5,6", "7");
