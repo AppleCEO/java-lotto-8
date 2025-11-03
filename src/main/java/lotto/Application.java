7be2507991c531fc1a6a83c5a323cde8931d0399
도미닉 <kiljh@me.com>
2025-11-03 12:05:07 +0900

    refactor: 로또 당첨 결과를 출력하는 역할을 담당하는 별도 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,26 +8,8 @@ public class Application {
     public static void main(String[] args) {
         LottoInput lottoInput = ConsoleInputReader.readAllInputs();
         List<Lotto> lottos = LottoFactory.createLottos(lottoInput.getMoney());
-        printResult(lottos, lottoInput);
+        LottoResultService.printResult(lottos, lottoInput);
     }
 
-    private static void printResult(List<Lotto> lottos, LottoInput lottoInput) {
-        Map<Ranking, Long> result = getRanking(lottos, lottoInput);
-        double profitRatio = getProfitRatio(result, lottoInput);
-        ConsoleOutput.printResult(result, profitRatio * 100);
-    }
 
-    private static Map<Ranking, Long> getRanking(List<Lotto> lottos, LottoInput lottoInput) {
-        Map<Ranking, Long> result = lottos.stream()
-                .map(lotto -> lotto.checkRanking(lottoInput.getWinningNumbers(), lottoInput.getMoney()))
-                .collect(Collectors.groupingBy(ranking -> ranking, Collectors.counting()));
-        return result;
-    }
-
-    private static double getProfitRatio(Map<Ranking, Long> result, LottoInput lottoInput) {
-        long totalPrizeMoney = result.entrySet().stream()
-                .mapToLong(entry -> entry.getKey().getPrizeMoney() * entry.getValue())
-                .sum();
-        return (double) totalPrizeMoney / lottoInput.getMoney();
-    }
 }
